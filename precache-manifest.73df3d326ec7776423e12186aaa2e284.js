5e00159b9d1d64faa9ee8a465e1d54bdd05fca7f
tsai <viabytsai@gmail.com>
2022-02-16 12:32:35 +0800

    deploy

---
+++ precache-manifest.73df3d326ec7776423e12186aaa2e284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd3008addfba4465d8f",
+    "url": "/moneymanger-website/css/app.5cd4b0ef.css"
+  },
+  {
+    "revision": "45f4c117dfe0197e5e7ee536eb69073a",
+    "url": "/moneymanger-website/index.html"
+  },
+  {
+    "revision": "3bd3008addfba4465d8f",
+    "url": "/moneymanger-website/js/app.669efb9b.js"
+  },
+  {
+    "revision": "1867600efa358b406c1c",
+    "url": "/moneymanger-website/js/chunk-vendors.4c6e3f14.js"
+  },
+  {
+    "revision": "c27aa7e108adec910c37eab85e864c15",
+    "url": "/moneymanger-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneymanger-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/moneymanger-website/precache-manifest.73df3d326ec7776423e12186aaa2e284.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "moneymanger"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
